4fbee89f3dd18575b87ef69117fe79590a44d411
FoggDev <foggdev@gmail.com>
2021-04-09 00:54:05 -0700

    Re-organizing functions

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,13 +1,78 @@
-import { getParams, pick, isLanguage } from '../index'
+import { isLanguage, isString, isDefined, isBrowser } from '../is'
+import { pick } from '../object'
+import { getLocation } from '../url'
 import { languagesList } from '../languages'
 
-export const getLanguagesList = () => {
-  return languagesList
+export function getParams(url: string, index = 0): string | string[] {
+  if (!url) {
+    url = getLocation().pathname
+  }
+
+  if (isString(url)) {
+    if (url.indexOf('?') > -1) {
+      url = url.substr(0, url.indexOf('?'))
+    }
+
+    const params = url.split('/')
+    params.shift()
+
+    if (params[params.length - 1] === '') {
+      params.pop()
+    }
+
+    if (index) {
+      if (isLanguage(params[0])) {
+        index += 1
+      }
+
+      if (isDefined(params[index])) {
+        return params[index]
+      }
+    }
+
+    return params
+  }
+
+  return ''
 }
 
 export const getCurrentLanguage = (url?: string, defaultLanguage = 'en-US') => {
   const params = getParams(url || '')
   return params && isLanguage(params[0]) ? params[0] : defaultLanguage
+}
+
+export function redirectTo(url = '/', includeLanguage?: any): void {
+  if (isBrowser()) {
+    const { pathname } = window.location
+    const language = getCurrentLanguage()
+    let slash = '/'
+
+    if (url === '_self') {
+      if (isLanguage(includeLanguage)) {
+        const segments = pathname.split(slash).filter(v => v)
+
+        if (isLanguage(segments[0])) {
+          segments[0] = includeLanguage
+        }
+
+        window.location.href = `${slash}${segments.join('/')}`
+      } else {
+        window.location.href = pathname
+      }
+    } else if (includeLanguage) {
+      if (url[0] === '/') {
+        slash = ''
+      }
+
+      window.location.href = `/${language}${slash}${url}`
+    } else {
+      window.location.href = url
+    }
+  }
+}
+
+export const getLanguagesList = () => {
+  return languagesList
 }
 
 export const getSelectLanguages = (list: any) => {
